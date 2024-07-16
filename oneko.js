7db62e07a66c3f2055b230429d04f3806eef75c0
Rozengerj <108734195+Rozengerj@users.noreply.github.com>
2024-07-16 16:27:09 +0300

    Update oneko.js

--- oneko.js
+++ oneko.js
@@ -18,7 +18,7 @@
   let idleAnimation = null;
   let idleAnimationFrame = 0;
 
-  const nekoSpeed = 100;
+  const nekoSpeed = 67;
   const spriteSets = {
     idle: [[-3, -3]],
     alert: [[-7, -3]],
